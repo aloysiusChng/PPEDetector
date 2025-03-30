d04f262b98572b28d57d2178d38b930711494cad
Aloysius Chng <2302857@sit.singaporetech.edu.sg>
2025-03-30 15:18:32 +0800

    Create config-example.py

---
+++ src/config-example.py
@@ -0,0 +1,8 @@
+class Config():
+    UPLOAD_ACCESS_KEY : str = "UPLOAD_ACCESS_KEY"
+    SQLALCHEMY_DATABASE_URI : str = "SQLALCHEMY_DATABASE_URI"
+
+    S3_ACCESS_KEY : str = "S3_ACCESS_KEY"
+    S3_SECRET_KEY : str = "S3_SECRET_KEY"
+    S3_REGION : str = "ap-southeast-1"
+    S3_BUCKET : str = "ppe-vision-image"
